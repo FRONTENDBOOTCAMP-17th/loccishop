c508123c34ba8565a407abc04820736a9ebc0e14
hyeonwoo1231 <252298490+hyeonwoo1231@users.noreply.github.com>
2026-04-06 15:51:20 +0900

    헤더 푸터 마크업 완료

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+export const headerComponent = () => html`
+  <header class="bg-header-footer shadow p-4 w-full h-16">
+    <div
+      class="header-inner h-full flex items-center justify-between max-inline-360 mx-auto"
+    >
+      <img src="/src/assets/icon/Hamburger.svg" alt="" />
+      <img src="/src/assets/logo/Logo.svg" alt="" class="" />
+      <ul class="flex gap-4">
+        <img src="/src/assets/icon/Location.svg" alt="" />
+        <img src="/src/assets/icon/FAQ.svg" alt="" />
+        <img src="/src/assets/icon/myPage.svg" alt="" />
+        <img src="/src/assets/icon/Items.svg" alt="" />
+      </ul>
+    </div>
+  </header>
+`;
