0cb45bac167c5fb6093f9948a1f3497bc1ebcc0e
hyeonwoo1231 <252298490+hyeonwoo1231@users.noreply.github.com>
2026-04-07 10:29:31 +0900

    fix:20260406 리뷰 반영

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,17 @@
-export const headerComponent = () => html`
-  <header class="bg-header-footer shadow p-4 w-full h-16">
-    <div
-      class="header-inner h-full flex items-center justify-between max-inline-360 mx-auto"
-    >
-      <img
-        src="/src/assets/icon/Hamburger.svg"
-        alt=""
-        class="hamburger-btn cursor-pointer"
-      />
-      <img src="/src/assets/logo/Loccitane.svg" alt="" class="" />
+export function renderHeader() {
+  const header = document.createElement("header");
+  header.className = "bg-header-footer shadow p-4 w-full h-16";
+
+  header.innerHTML = `
+    <div class="header-inner h-full flex items-center justify-between max-inline-360 mx-auto">
+      <img src="/src/assets/icon/Hamburger.svg" alt="" class="hamburger-btn cursor-pointer" />
+      <img src="/src/assets/logo/Loccitane.svg" alt="" />
       <ul class="flex gap-4">
         <img src="/src/assets/icon/myPage.svg" alt="" />
         <img src="/src/assets/icon/Items.svg" alt="" />
       </ul>
     </div>
-  </header>
-`;
+  `;
+
+  return header;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import "../styles/style.css";
 
-import { createDrawer } from "../components/ui/drawer.js";
+import { createDrawer } from "/src/components/ui/drawer.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   const menuDrawer = createDrawer({
@@ -10,4 +10,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const hamburgerBtn = document.querySelector(".hamburger-btn");
   menuDrawer.content.innerHTML = `<div class="p-4">록시땅 메뉴 내용물 테스트</div>`;
+});
+
+import { renderHeader } from "../components/header.js";
+import { renderFooter } from "../components/footer.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("header").append(renderHeader());
+  document.getElementById("footer").append(renderFooter());
 });
